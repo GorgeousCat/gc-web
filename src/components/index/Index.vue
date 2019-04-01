e23a042f1871f7df97e410ee3d97f7243e47fb7c
GorgeousCat <1477064558@qq.com>
2019-04-01 19:14:11 +0800

    理解routerlink以及隐藏头部和底部

---
+++ src/components/index/Index.vue
@@ -0,0 +1,21 @@
+<template>
+    <p>login</p>
+</template>
+
+<script>
+    export default {
+        name: "index",
+      data(){
+          return {
+            headerShow: true,
+          }
+      },
+      created() {
+          this.$emit('header',true);
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
